292669abc44838a363ece9cb89261a8c74cb65fd
aftabaslam <formanite.2000@gmail.com>
2016-08-23 14:41:43 +0500

    New changelist

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.use(function(req, res, next) {
 
 /** API path that will upload the files */
 app.post('/upload',upload ,function (req, res) {
-  console.log('=====>>>>>>>',req.body);
+  console.log('=====>>>>>>>',req.body.file);
   res.json({status:200,data:req.file})
 });
 
