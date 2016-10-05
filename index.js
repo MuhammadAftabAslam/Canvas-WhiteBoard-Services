9633539f71952d26ff87f8619790cb0631287760
aftabaslam <formanite.2000@gmail.com>
2016-10-05 22:29:02 +0500

    Change CORs

--- index.js
+++ index.js
@@ -47,8 +47,10 @@ var upload = multer({ //multer settings
 
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
   next();
 });
 
